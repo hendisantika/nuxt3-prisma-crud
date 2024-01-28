6dfbdfe4374df717ae4999a8dfc4a79fe4434288
hendisantika <hendisantika@yahoo.co.id>
2024-01-29 05:32:23 +0700

    Add required files 🫸🌀✏️📗🐧🐳

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: {enabled: false},
+    css: ['~/assets/css/tailwind.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    // imports: {
+    //   dirs: ['stores'],
+    // },
+    // modules: [
+    //   [
+    //     '@pinia/nuxt',
+    //     {
+    //       autoImports: ['defineStore', 'acceptHMRUpdate'],
+    //     },
+    //   ]
+    // ],
+})
